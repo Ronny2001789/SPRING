2e720b09b2d8b31eeec112da7b0d404c3957931d
Ronny2001789 <ronaldmputla25@gmail.com>
2025-12-16 13:23:03 +0200

    s

--- tacos05/data/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package tacos.tacos05.data;
-import org.springframework.data.repository.CrudRepository;
-import tacos.tacos05.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-  User findByUsername(String username);
-  
-}
